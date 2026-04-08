2a5439c6d5234696e8bf8b030ab6d4506eed19fc
Adriceka <adrigonzalezgonzalez2007@gmail.com>
2026-04-08 09:22:01 +0200

    feat: implementar clase principal
    implementar clase principal BiciPalma con casos de prueba

---
+++ src/main/java/bicipalma/appalma/BiciPalma.java
@@ -0,0 +1,45 @@
+package bicipalma.appalma;
+
+import bicipalma.bicicleta.Bicicleta;
+import bicipalma.estacion.Estacion;
+import bicipalma.tarjetaUsuario.TarjetaUsuario;
+
+public class BiciPalma {
+
+	public static void main(String[] args) {
+		
+		Estacion estacion = new Estacion(1, "Manacor", 6);
+		
+		System.out.println("\n **** caso TEST visualizar estado de la estacion **** \n");
+		estacion.consultarEstacion();
+				
+		System.out.println("\n **** caso TEST visualizar anclajes libres **** \n");
+		System.out.println("anclajesLibres: " + estacion.anclajesLibres());
+
+		System.out.println("\n **** caso TEST anclar bicicleta(s) **** \n");
+		
+		int[] bicicletas = {291, 292, 293, 294};
+		
+		for (int id : bicicletas) {
+			Bicicleta bicicleta = new Bicicleta(id);
+			estacion.anclarBicicleta(bicicleta);
+		}
+
+		System.out.println("anclajesLibres tras generar " + bicicletas.length + " bicis: " + estacion.anclajesLibres());
+		
+		System.out.println("\n **** caso TEST consultar bicicletas ancladas **** \n");
+		estacion.consultarAnclajes();
+		
+		System.out.println("\n **** caso TEST retirar bicicleta **** \n");
+		
+		TarjetaUsuario tarjetaUsuario = new TarjetaUsuario("000456789", true);
+		
+		System.out.println("¿tarjeta de usuario activada? (true/false): " + estacion.leerTarjetaUsuario(tarjetaUsuario));
+		
+		estacion.retirarBicicleta(tarjetaUsuario);
+		
+		estacion.consultarAnclajes();
+		
+		System.out.println("anclajesLibres: " + estacion.anclajesLibres());
+	}
+}
